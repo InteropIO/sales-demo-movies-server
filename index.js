ab8e81c74acd5a6ed82a236913efc3335c9867e4
Kostadin Belchev <kostadin.belchev@tick42.com>
2024-02-02 12:08:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// add movies endpoint
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app; // Export the Express app
