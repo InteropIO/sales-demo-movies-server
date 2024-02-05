acf0c5594e7198cb40da68462de8d9492768eadc
Kostadin Belchev <kostadin.belchev@tick42.com>
2024-02-05 17:23:09 +0200

    read movies from file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-let movies = '/data/movies.json';
+const fs = require('fs');
+
+const movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel TEST");
